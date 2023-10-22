41e0ef5a30305e81a9f562da3d4e373f13c88644
IsaacMellonie <izeymelon@yahoo.com>
2023-10-22 22:50:20 +1000

    Add to README setup guide.

---
+++ src/email_setup.py
@@ -0,0 +1,2 @@
+my_email = "isaac.e.mellonie@gmail.com" # <<< Put your own Gmail here
+my_password = "dzms iuxs xxzz iaew" # <<< Put your own password here
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import datetime, ssl, smtplib 
 from email.message import EmailMessage
-from my_password import my_password
+from email_setup import my_password, my_email
 # app password = dzms iuxs xxzz iaew
 
 # Format date and time for display in Terminal
@@ -23,8 +23,8 @@ else:
 smtp_port = 587
 smtp_server = "smtp.gmail.com"
 
-email_from = "isaac.e.mellonie@gmail.com"
-email_to = "isaac.e.mellonie@gmail.com"
+email_from = my_email
+email_to = "isaaceveans@gmail.com"
 subject = "Parking Receipt"
 body_text = "This is a test of the new emailing system."
 message = "Subject: {}\n\n{}".format(subject, body_text)
@@ -32,6 +32,7 @@ message = "Subject: {}\n\n{}".format(subject, body_text)
 password = my_password
 
 simple_email_context = ssl.create_default_context()
+
 try:
     print("Connecting to the server...")
     parking_server = smtplib.SMTP(smtp_server, smtp_port)
@@ -40,9 +41,9 @@ try:
     print("Connected to server :)")
 
     print()
-    print(f"Sending email to - {email_to}")
+    print(f"Sending email from - {email_from}")
     parking_server.sendmail(email_from, email_to, message)
-    print(f"Email sent to - {email_from}")
+    print(f"Email sent to - {email_to}")
 
 except Exception as e:
     print(e)
@@ -50,39 +51,4 @@ except Exception as e:
 finally:
     parking_server.quit()
 
-# email_sender = "isaac.e.mellonie@gmail.com"
-# email_password = my_password
-
-# email_receiver = "isaaceveans@gmail.com"
-
-# subject = "Parking ticket application email test"
-# body = """
-# This is an initial test of the applications emailing system.
-# """
-
-# em = EmailMessage()
-# em ["From"] = email_sender
-# em["To"] = email_receiver
-# em["subject"] = subject
-# em.set_content(body)
-
-# context = ssl.create_default_context()
-
-# with smtplib.SMTP_SSL("smpt.gmail.com", 465, context=context) as smtp:
-#     smtp.login(email_sender, email_password)
-#     smtp.sendmail(email_sender, email_receiver, em.as_string())
-
-
-
-
-# try:
-#     server_ssl = smtplib.SMTP_SSL("smtp.yahoo.com", 465)
-#     server_ssl.ehlo() 
-# except:
-#     "Something went wrong"
-
-# sender = "izeymelon@yahoo.com"
-# receiver = "isaaceveans@gmail.com"
-# subject = "Hey, this is the parking app!"
-# body = "Hey there! Welcome aboard."
 
