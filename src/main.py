530d45a76699ea399185b6a68315aa8e23d4be63
IsaacMellonie <izeymelon@yahoo.com>
2023-10-22 14:14:42 +1000

    add datetime and welcome depending on time of day

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
 import datetime
 
+
+today_date = datetime.datetime.now()
+formatted_date = today_date.strftime("%d/%m/%y \n%X %p")
+print(formatted_date)
+
+today_date = datetime.datetime.now()
+current_time = datetime.datetime.now()
+current_time.hour
+
+
+if current_time.hour < 12:
+    print("Goodmorning!")
+elif 12 <= current_time.hour < 18:
+    print("Good Afternoon!")
+else:
+    print("Good evenning!")
