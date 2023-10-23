41a9bc4f8eb838165bd091d5b46892b198020d21
IsaacMellonie <izeymelon@yahoo.com>
2023-10-23 14:10:07 +1000

    change clock display

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from email_setup import my_password, my_email
 
 # Format date and time for display in Terminal
 today_date = datetime.datetime.now()
-formatted_date = today_date.strftime("%d/%m/%Y \n%X %p")
+formatted_date = today_date.strftime("%d/%m/%Y \n%-I:%M %p")
 print(formatted_date)
 
 # Get time as int from datetime.datetime
@@ -20,35 +20,35 @@ elif 12 <= current_time.hour < 18:
 else:
     print("Good evenning!")
 
-smtp_port = 587
-smtp_server = "smtp.gmail.com"
+# smtp_port = 587
+# smtp_server = "smtp.gmail.com"
 
-email_from = my_email
-email_to = "isaaceveans@gmail.com"
-subject = "Parking Receipt"
-body_text = "This is a test of the new emailing system."
-message = "Subject: {}\n\n{}".format(subject, body_text)
+# email_from = my_email
+# email_to = "isaaceveans@gmail.com"
+# subject = "Parking Receipt"
+# body_text = "This is a test of the new emailing system."
+# message = "Subject: {}\n\n{}".format(subject, body_text)
 
-password = my_password
+# password = my_password
 
-simple_email_context = ssl.create_default_context()
+# simple_email_context = ssl.create_default_context()
 
-try:
-    print("Connecting to the server...")
-    parking_server = smtplib.SMTP(smtp_server, smtp_port)
-    parking_server.starttls(context=simple_email_context)
-    parking_server.login(email_from, password)
-    print("Connected to server :)")
+# try:
+#     print("Connecting to the server...")
+#     parking_server = smtplib.SMTP(smtp_server, smtp_port)
+#     parking_server.starttls(context=simple_email_context)
+#     parking_server.login(email_from, password)
+#     print("Connected to server :)")
 
-    print()
-    print(f"Sending email from - {email_from}")
-    parking_server.sendmail(email_from, email_to, message)
-    print(f"Email sent to - {email_to}")
+#     print()
+#     print(f"Sending email from - {email_from}")
+#     parking_server.sendmail(email_from, email_to, message)
+#     print(f"Email sent to - {email_to}")
 
-except Exception as e:
-    print(e)
+# except Exception as e:
+#     print(e)
 
-finally:
-    parking_server.quit()
+# finally:
+#     parking_server.quit()
 
 
