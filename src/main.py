4be7c18d09073f630224515544f767075057a308
IsaacMellonie <izeymelon@yahoo.com>
2023-10-27 21:42:31 +1000

    create a function for creating csv records that adds rows for each of the user details.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
-import datetime, ssl, smtplib, time, hashlib, email_system, get_user_info, re
+import datetime, ssl, smtplib, time, hashlib, email_system, get_user_info, re, os, csv
 from email.message import EmailMessage
 from login_system import signup, login
 from email_setup import my_password, my_email
 from purchase import GetTime
+
+
+def create_csv():
+    records_filename = "login_details_enhanced.csv"
+    def create_records_csv(records_filename):
+        # Check if the file already exists
+        if not os.path.exists(records_filename):
+            # Create and write the header to the csv file
+            with open(records_filename, 'w', newline="") as file:
+                csvwriter = csv.writer(file)
+                print("Creating a header row for the file")
+                csvwriter.writerow([
+                    "email",
+                    "password", 
+                    "first", 
+                    "last", 
+                    "rego", 
+                    "ticket",
+                ])
+                print(f"Created a .CSV file to store employee data named {records_filename}")
+        else:
+            pass
+    create_records_csv(records_filename)
 
 # Format date and time for display in Terminal
 def time_date():
@@ -27,7 +50,7 @@ def welcome():
 def open_menu():
     current_time = datetime.datetime.now()
     i = 0
-    while i == 0 and 7 <= current_time.hour < 19:
+    while i == 0 and 7 <= current_time.hour < 24:
         i = int(input("Welcome to the Parking Pal App\n\nPress 1 to register.\nPress 2 to signin.\nPress 3 if you forgot your password.\nPress 4 to exit.\n"))
         if i == 1: # register
             register()
@@ -65,51 +88,49 @@ def open_menu():
 
 # Function to buy tickets after the user has logged in. They'll have to enter a 16 digit credit card number and expiry date.
 def get_ticket():
-    while True:
-            cc = input("Please enter a fake 16 digit credit card number: ")
-            if len(cc) != 16:
-                print("Credit card must be 16 digits. Try again?")
-                choice = input("1 for yes, 2 for no: ")
-                if choice == '2':
-                    break
-            try:
-                cc = int(cc)
-            except ValueError:
-                print("Numbers only, please.")
+    valid_card = 0
+    while valid_card == 0:
+        try:
+            cc = int(input("Please enter a fake 16 digit credit card number: "))
+            if len(str(cc)) == 16:
+                valid_card = 1
                 continue
-            time.sleep(1)
-            print("You entered:", cc)
-            while True:
-                    expiry_month = int(input("Please enter a 2 digit month (e.g. MM):"))
-                    if type(expiry_month) == int and len(expiry_month) == 2:
-                        expiry_year = int(input("Please enter a 2 digit year (e.g. YY):"))
-                        if type(expiry_year) == int and len(expiry_year) == 2:
-                            print("Thank you.")
-                            minutes = int(input("Please enter the amount of minutes you'd like to purchase.\nMax is 120 mins. Min is 5 mins: "))
-                            answer = int(input(f"Is this the corect amount of minutes? {minutes}\n1 for yes or 2 for no."))
-                            if answer == 1:
-                                print("You havea total of {minutes}")
-                                GetTime(minutes)
-                                # Get the user details from login_details.csv and send the receipt of purchase
-                            else:
-                                print("Ok, let's try again!")
-                        try:
-                            expiry_month = int
-                        except ValueError:
-                            print("Sorry, please enter numbers only in the correct format.")
-                            continue
-                    try:
-                        expiry_month = int
-                    except ValueError:
-                        print("Sorry, please enter numbers only in the correct format.")
-                        continue
+            else:
+                print("Credit card must be 16 digits.")
+        except ValueError:
+            print("Numbers only, please.")
+            continue
+    time.sleep(1)
+    print("You entered:", cc)
+    valid_card = 1
+    while True:
+            expiry_month = int(input("Please enter a 2 digit month (e.g. MM):"))
+            if type(expiry_month) == int and len(expiry_month) == 2:
+                expiry_year = int(input("Please enter a 2 digit year (e.g. YY):"))
+                if type(expiry_year) == int and len(expiry_year) == 2:
+                    print("Thank you.")
+                    minutes = int(input("Please enter the amount of minutes you'd like to purchase.\nMax is 120 mins. Min is 5 mins: "))
+                    answer = int(input(f"Is this the corect amount of minutes? {minutes}\n1 for yes or 2 for no."))
+                    if answer == 1:
+                        print("You havea total of {minutes}")
+                        GetTime(minutes)
+                        # Get the user details from login_details.csv and send the receipt of purchase
                     else:
-                        print("\nThank you. Have a great day!")
-                        time_date()
-                        welcome()
-                        open_menu()
+                        print("Ok, let's try again!")
+                try:
+                    expiry_month = int
+                except ValueError:
+                    print("Sorry, please enter numbers only in the correct format.")
+                    continue
+            try:
+                expiry_month = int
+            except ValueError:
+                print("Sorry, please enter numbers only in the correct format.")
+                continue
+            else:
+                print("\nThank you. Have a great day!")
+                return minutes
                     
-    
 def login():
     i = 1
     while i == 1:
@@ -136,7 +157,7 @@ def login():
             except Exception:
                 print("Please enter a number value 1 or 2.")
 
-def is_email_valid (email):
+def is_email_valid(email):
     return bool(re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email))
 
 def get_valid_email():
@@ -154,7 +175,7 @@ def register():
         password = input("Enter password: ")
         conf_pwd = input("Confirm password: ")
         if conf_pwd == password:
-            with open("login_details.csv", "w") as f:
+            with open("login_details.csv", "w") as f: # Save details as a list instead.
                     f.write(email + "\n" + password)
             f.close()
             print("You're now successfully registered.")
@@ -191,8 +212,6 @@ def register():
             print("\nPasswords don't match. Please try again.\n")
             time.sleep(1)
 
-
-
 def loggedin():
     while True:
         try:
@@ -207,6 +226,7 @@ def loggedin():
             print("Invalid input. Please enter a valid number.")
 
 time_date()
+output_var = welcome()
 welcome()
 open_menu()
 
