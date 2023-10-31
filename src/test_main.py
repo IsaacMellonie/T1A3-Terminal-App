c6128d7ca355721281ec5eeca74726f7dfcbe650
IsaacMellonie <izeymelon@yahoo.com>
2023-10-31 11:23:00 +1000

    updates to README resources and installation

--- src/test_main.py
+++ src/test_main.py
@@ -2,12 +2,14 @@ from main import is_email_valid
 import pytest
 
 
+# Testing that if the email check passes it returns a True value.
 def test_is_email_valid():
     example_email = "mrbig@gmail.com"
     assert is_email_valid(example_email) == True
     
 
+# Testing that if the email check fails it returns a False value.
 def test_is_email_valid_2():
     example_email_2 = "asdadsfasda234234"
-    assert is_email_valid(example_email_2) == True
+    assert is_email_valid(example_email_2) == False
 
